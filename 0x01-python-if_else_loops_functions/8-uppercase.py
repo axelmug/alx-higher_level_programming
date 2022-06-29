5e2dc710899d22b113b0279dbf7cee922af16599
Mugisha Axel <58776846+axelmug@users.noreply.github.com>
2022-06-29 00:49:21 +0000

    file 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    letter = ""
+    for i in str:
+        c = ord(i)
+        if ord('a') <= c <= ord('z'):
+            c = c - 32
+        letter += chr(c)
+    print("{}".format(letter))
+    
