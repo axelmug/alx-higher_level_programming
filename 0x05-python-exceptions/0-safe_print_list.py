e54199bcb96a69456510d03c2471eda07723faa1
root <root@Axel.localdomain>
2022-07-20 23:38:15 +0200

    Fix 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,12 +10,12 @@ def safe_print_list(my_list=[], x=0):
     Returns:
     The actual number of elements printed
     '''
-    x = 0
-    for i in range(y):
+    n = 0
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             n += 1
         except Exception:
             break
     print("")
-    return x
+    return n
